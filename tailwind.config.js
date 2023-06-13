5095a94bfe229113a93b6533e2c71f5b03bc8364
Peter Heisig <peter@heisig.rocks>
2023-06-13 15:20:43 +0000

    home done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,15 @@
 const typography = require('@tailwindcss/typography');
+const forms = require('@tailwindcss/forms');
 
 module.exports = {
 	content: ['./hugo_stats.json'],
-	plugins: [typography],
+	plugins: [forms, typography],
 	theme: {
 		extend: {
 			colors: {
 				'primary': '#2f435b',
 				'secondary': '#00939B',
 				'tertiary': '#E9EEFE'
-			},
-			scale: {
-				'-100': '-1',
 			}
 		}
 	}
